49e2960da264414b55a5141ce47d885c4a0c02d4
mediacyt <mediaman083@hotmail.com>
2017-11-12 19:10:48 -0500

    Update MSLNLAudio.js

--- MSLNLAudio.js
+++ MSLNLAudio.js
@@ -112,8 +112,7 @@ if (!window[windowChannelIndex]) {
             Seinfeld: "https://www.myinstants.com/media/sounds/seinfeld-season-7-closing-credits.mp3",
             GetToDeChoppa: "https://www.myinstants.com/media/sounds/get-to-the-choppa-baby_1_1.mp3",
             Splurt: "https://www.myinstants.com/media/sounds/splurt.mp3"
-            
-        }, window[windowChannelIndex].audioLibrary ? window[windowChannelIndex].audioLibrary.squees : undefined);
+            }, window[windowChannelIndex].audioLibrary ? window[windowChannelIndex].audioLibrary.squees : undefined);
         this.handler = {
             Squee: function(data) {
                 var squee;
